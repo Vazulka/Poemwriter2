94affd566e31756d70efbaab495f9bafbdfebe35
Vazulka <vazulka@freemail.hu>
2017-03-10 13:27:46 +0100

    +menu base

--- poemwriter2/poemwriter2/MainWindow.xaml.cs
+++ poemwriter2/poemwriter2/MainWindow.xaml.cs
@@ -25,6 +25,24 @@ namespace poemwriter2
             InitializeComponent();
         }
 
-
+        private void move(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+         private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
